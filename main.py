8e7fb0767bb3109af9824ef864f7bbd3d8401a8e
akhil10996 <57706981+akhil10996@users.noreply.github.com>
2022-02-12 14:59:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ people = int(input("How many people to split the bill?"))
 tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
+#dividing the total bill to the number of people
 bill_per_person = total_bill / people
 #rounding it to 2 decimals
 final_amount = round(bill_per_person, 2)
